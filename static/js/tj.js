1d965d252ce990aa08a8861f4486c99ecff06950
root <root@debian.debian>
2022-08-20 22:05:49 -0400

    first commit

---
+++ static/js/tj.js
@@ -0,0 +1,11 @@
+document.write('<script data-cfasync="false" async src="//aparvibices.com/fJeKTumbGithrkX2/39227"></script>
+<script data-cfasync="false" async  src="//cuseccharm.com/ryJcfjCuoYWcdXq/39226"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TEZS3SZ3PW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag(\'js\', new Date());
+
+  gtag(\'config\', \'G-TEZS3SZ3PW\');
+</script>')
